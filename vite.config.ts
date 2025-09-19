f8db5bcf9bd668fa92b1537bba182f205ea1d156
venomxp <65331189+venomxp@users.noreply.github.com>
2025-09-19 14:23:08 +0100

    Fix deployment configuration for Bolt Hosting

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Set the base path for GitHub Pages deployment.
-  // This should match the name of your repository.
-  base: '/cherifa_falak/',
 })
