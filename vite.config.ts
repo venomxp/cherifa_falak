4017b9ca83c49296ae1ae7d2119c948cf649497d
venomxp <65331189+venomxp@users.noreply.github.com>
2025-09-19 14:02:37 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // Set the base path for GitHub Pages deployment.
   // This should match the name of your repository.
-  base: '/falkom/',
+  base: '/cherifa_falak/',
 })
